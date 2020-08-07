8582864648e8f1c720fa5cb5b909a23a47a66ba7
Keshav Desai <keshav.desai@gmail.com>
2020-08-07 11:50:48 +0100

    Updated

--- unsupportedMethod.groovy
+++
@@ -1,8 +0,0 @@
-import com.sap.gateway.ip.core.customdev.util.Message;
-import java.util.HashMap;
-
-def Message processData(Message message) {
-	 
-    throw new Exception("Unsupported method: "+message.getHeaders().get("CamelHTTPMethod"));
-
-}
