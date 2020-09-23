7939b66e0e7af302894ade61a3ec9a93e994c36e
Keshav Desai <keshav.desai@gmail.com>
2020-09-23 16:44:40 +0100

    eclipse changes - 4

---
+++ groovy/src/main/java/groovy/SyntaxBasics.groovy
@@ -0,0 +1,19 @@
+package groovy
+
+/*
+ USE alt+shift+x and press G to run as Groovy Script
+ */
+
+
+println "hello - prints newline after texts"
+
+//below doesnt print in newline,unless explicitly specified
+print "hello again - 1"
+print "hello again - 2"+'\n'
+
+println("brackets make me print in a new line")
+
+//single quotes, double quotes dont matter?	apparently String and GString
+def somestr = 'somestring'
+println "$somestr".getClass()
+println somestr.getClass()
