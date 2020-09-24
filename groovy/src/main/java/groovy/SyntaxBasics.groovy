81ce04c2bd5f317fb2ea8fbde4e8839bca0d3d3c
Keshav Desai <keshav.desai@gmail.com>
2020-09-24 10:55:20 +0100

    202009241054

--- groovy/src/main/java/groovy/SyntaxBasics.groovy
+++ groovy/src/main/java/groovy/SyntaxBasics.groovy
@@ -1,6 +1,6 @@
 package groovy
 
-/*
+/**@
  USE alt+shift+x and press G to run as Groovy Script
  */
 
@@ -15,5 +15,41 @@ println("brackets make me print in a new line")
 
 //single quotes, double quotes dont matter?	apparently String and GString
 def somestr = 'somestring'
-println "$somestr".getClass()
+println "$somestr".getClass() //use $ as prefix to variable when being used 
+                              //inside quotes - avoid concatenation of str
 println somestr.getClass()
+
+//-----testing groovydoc - disabled by default------
+
+
+/**@
+ * Some class groovydoc for Foo
+ */
+class Foo {
+	/**@
+	 * Some method groovydoc for bar
+	 */
+	void bar() {
+	}
+}
+
+//println this.class.getGroovydoc()
+
+//-----testing groovydoc ----------------------------
+
+//Quoted identifiers -- Maps - this is how the headers and body are returned 
+//in CPI
+
+def map = [:]
+
+map."somekey"="someval"
+map."newkey" = "newval"
+map.'onekey'='oneval'
+map.name = 'somename'
+
+
+map.put('key1', 'val1')
+
+println map
+
+println map.get('name')
